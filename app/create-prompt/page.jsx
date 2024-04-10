0ed142c983ec69693fa02e721cc48fc89f71fde1
ShubhaamTiwary <shubhaamtiwary@gmail.com>
2024-04-11 01:15:01 +0530

    Create post frontend

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,9 +5,43 @@ import { useRouter } from 'next/router'
 import Form from '@components/Form'
 
 const CreatePrompt = () => {
-    const [submitting,setSubmitting]=useState(false)
+  const [submitting, setSubmitting] = useState(false)
+  const [post,setPost]  = useState({
+    prompt: '',
+    tag:'',
+  })
+  const createPrompt= async(e)=>{
+    e.preventDefault()
+    setSubmitting(true)
+    try{
+      const res = await fetch('/api/prompt/new',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session.user.id,
+          tag: post.tag
+        })
+      })
+      if(!res.ok){
+        router.push('/')
+      }   
+    }catch(e){
+      throw Error(e.message)
+    }
+    finally{
+      setSubmitting(false)
+    }
+  }
   return (
-    <Form 
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
     />
   )
 }
